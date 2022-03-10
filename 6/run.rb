5a5a70062118f566ce6d7776168201727b0c1a0c
Matt Creenan <mattcreenan@gmail.com>
2022-03-10 09:40:17 -0500

    Remove some code by using rotate on array to move values in hash

--- 6/run.rb
+++ 6/run.rb
@@ -6,15 +6,9 @@ empty_fish_counts = (0..8).zip([].fill(0, 0..8)).to_h
     fish_counts = empty_fish_counts.merge(all_fish.tally)
     (1..days).each do
         new_fish_count = fish_counts[0]
-        fish_counts[0] = fish_counts[1]
-        fish_counts[1] = fish_counts[2]
-        fish_counts[2] = fish_counts[3]
-        fish_counts[3] = fish_counts[4]
-        fish_counts[4] = fish_counts[5]
-        fish_counts[5] = fish_counts[6]
-        fish_counts[6] = fish_counts[7] + new_fish_count
-        fish_counts[7] = fish_counts[8]
-        fish_counts[8] = new_fish_count
+        fish_counts = fish_counts.keys.zip(fish_counts.values.rotate).to_h
+        fish_counts[6] += new_fish_count
+        fish_counts[8]  = new_fish_count
     end
     puts fish_counts.values.sum
 end
